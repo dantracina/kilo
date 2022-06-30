7762274c35389fd1a7bfc843464b4b8ef9d191bf
Daniel <dantracina@gmail.com>
2022-06-30 10:46:33 -0300

    Desligamos a impressão na tela quando uma tecla é pressionada com ECHO e estamos lendo os caracteres um por um com ICANON ao invés de frase por frase

--- kilo.c
+++ kilo.c
@@ -1,7 +1,28 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+
+struct termios orig_termios; /* Variável que guardará os atributos originais do terminal */
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode); /* Função interessante do stlib.h, "NA SAIDA", pois quebra implicitamente o fluxo do programa */
+	
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON); 
+	
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+	enableRawMode();
+	
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1); /* Lê um 1 byte por vez, até o final e retorna o número de bytes lidos */
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); /* Lê um 1 byte por vez até o final e retorna o número de bytes lidos */
+	
 	return 0;
 }
