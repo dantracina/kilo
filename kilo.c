0363442a195126ac190510a9e01f47406aeb8667
Daniel <dantracina@gmail.com>
2022-06-30 10:00:28 -0300

    A começar a configuração do modo bruto do terminal

--- kilo.c
+++ kilo.c
@@ -1,3 +1,7 @@
+#include <unistd.h>
+
 int main() {
-		return 0;
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1); /* Lê um 1 byte por vez, até o final e retorna o número de bytes lidos */
+	return 0;
 }
